868e7bd92a64f3e30a249c44069a46ca2b2a1a78
Ali <muhammetalibegenc@gmail.com>
2023-06-04 22:50:10 +0300

    01/06/23

--- src/test/java/Practice/Odev.java
+++ src/test/java/Practice/Odev.java
@@ -1,4 +1,62 @@
 package Practice;
 
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Odev {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("chromeDriver", "src/resources/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        // GetMethods
+        //Amazon sayfasina gidelim. https://www.amazon.com//
+       driver.get("https://www.amazon.com");
+        Thread.sleep(3000);
+        //Sayfa basligini(title) yazdirin
+        System.out.println("Sayfa Basligi --> " + driver.getTitle());
+        Thread.sleep(3000);
+        //Sayfa basliginin "Amazon" icerdigini test edin
+          String actualtitle=driver.getTitle();
+          String expectedtitle ="Amazon";
+          if (actualtitle.contains(expectedtitle)){
+              System.out.println("Test PASSED");
+          }else {
+              System.out.println("Test FAILED");
+          }
+        Thread.sleep(3000);
+
+        //Sayfa adresini(url) yazdirin
+        System.out.println("Sayfa Url -->" + driver.getCurrentUrl());
+
+
+        //Sayfa url'inin "amazon" icerdigini test edin.
+        String actualUrl=driver.getCurrentUrl();
+        String expectedUrl = "amazon";
+
+        if (actualUrl.contains(expectedUrl)){
+            System.out.println("Test PASSED");
+        }else{
+            System.out.println("Test failed");
+        }
+        Thread.sleep(3000);
+        // Sayfa handle degerini yazdirin
+        System.out.println("Amazonun handle degeri --> " + driver.getWindowHandle());
+
+        Thread.sleep(3000);
+
+        // Sayfayi kapatin.
+        driver.close();
+
+
+
+
+
+
+    }
 }
---
+++ src/test/java/techproed/day06_relativeLocators/C01_relativeLocators.java
@@ -0,0 +1,73 @@
+package techproed.day06_relativeLocators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
+public class C01_relativeLocators {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        //amazona gidelim
+        driver.get("https://amazon.com");
+
+        //city bike aratalım
+        WebElement aramaKutusu = driver.findElement(By.xpath("//*[@type='text']"));
+        aramaKutusu.sendKeys("city bike", Keys.ENTER);
+
+
+        //relative locator kullanarak hybrid bikes altındaki elemente tıklayalım
+        //Webelementlerin yazısını konsola yazdıralım
+        WebElement hybridBike = driver.findElement(By.xpath("//*[text()='Hybrid Bikes']"));
+        System.out.println(hybridBike.getText());
+        WebElement electricBike = driver.findElement(with(By.tagName("a")).below(hybridBike));
+        System.out.println(electricBike.getText());
+        WebElement completeBike = driver.findElement(with(By.tagName("a")).below(electricBike));
+        System.out.println(completeBike.getText());
+
+        //Bikes altindaki tum linkleri (webelement) konsola yazdiralim
+        List<WebElement> linklerlistesi = driver.findElements(By.xpath("(//*[@class='a-list-item'])[position()>=10 and position()<=16]"));
+
+        /*
+
+        XPATH kullaniminda bir sayfadaki webelemntleri
+        mudahele etmek istedigimizde xpath ile locate aldigimizda birden fazla sonuc verebilir.
+        [position()>=10 and position()<=16]"))--> bu kullanimla aralik belirterek istedigimiz web secip mudahele eder
+                 */
+
+
+        //lambda ile yazdirdik
+        linklerlistesi.forEach(t -> System.out.println(t.getText()));
+        System.out.println("**************************************");
+
+        for (WebElement w : linklerlistesi) {
+            System.out.println(w.getText());
+        }
+
+        //Bu linklerinlerin hepsini tiklayalim ve sayfa basliklarini alalim
+
+        for (int i = 0; i < linklerlistesi.size(); i++) {
+            for (i = 0; i < linklerlistesi.size(); i++) {
+                linklerlistesi = driver.findElements(By.xpath("(//*[@class='a-list-item'])[position()>=9 and position()<=15]"));
+                System.out.println(linklerlistesi.get(i).getText());
+                linklerlistesi.get(i).click();
+                Thread.sleep(3000);
+                driver.navigate().back();
+                Thread.sleep(3000);
+            }
+
+
+            //completeBike.click();
+        }
+    }
+}
+
